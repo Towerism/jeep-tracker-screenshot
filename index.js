5d3163a7c74305848bb0433185ed881dd8c89d20
Martin Fracker, Jr <martinfrackerjr@gmail.com>
2022-09-05 16:51:11 -0500

    Instantiate new browser on every request

--- index.js
+++ index.js
@@ -9,54 +9,6 @@ const router = new Router();
 
 const TASK_QUEUE_SIZE = process.env.TASK_QUEUE_SIZE || 10;
 
-let browser;
-(async () => {
-  browser = await puppeteer.launch({
-    userDataDir: ".puppeteercache",
-    headless: true,
-    args: [
-      "--no-sandbox",
-      "--autoplay-policy=user-gesture-required",
-      "--disable-background-networking",
-      "--disable-background-timer-throttling",
-      "--disable-backgrounding-occluded-windows",
-      "--disable-breakpad",
-      "--disable-client-side-phishing-detection",
-      "--disable-component-update",
-      "--disable-default-apps",
-      "--disable-dev-shm-usage",
-      "--disable-domain-reliability",
-      "--disable-extensions",
-      "--disable-features=AudioServiceOutOfProcess",
-      "--disable-hang-monitor",
-      "--disable-ipc-flooding-protection",
-      "--disable-notifications",
-      "--disable-offer-store-unmasked-wallet-cards",
-      "--disable-popup-blocking",
-      "--disable-print-preview",
-      "--disable-prompt-on-repost",
-      "--disable-renderer-backgrounding",
-      "--disable-setuid-sandbox",
-      "--disable-speech-api",
-      "--disable-sync",
-      "--hide-scrollbars",
-      "--ignore-gpu-blacklist",
-      "--metrics-recording-only",
-      "--mute-audio",
-      "--no-default-browser-check",
-      "--no-first-run",
-      "--no-pings",
-      "--no-sandbox",
-      "--no-zygote",
-      "--password-store=basic",
-      "--use-gl=swiftshader",
-      "--use-mock-keychain",
-    ],
-    timeout: 10000,
-  });
-  console.log("puppeteer ready");
-})();
-
 async function getScreenshot(von, lastName) {
   const baseUrl =
     process.env.NODE_ENV === "development"
@@ -64,6 +16,49 @@ async function getScreenshot(von, lastName) {
       : "https://jeeponorder.com";
 
   try {
+    const browser = await puppeteer.launch({
+      userDataDir: ".puppeteercache",
+      headless: true,
+      args: [
+        "--no-sandbox",
+        "--autoplay-policy=user-gesture-required",
+        "--disable-background-networking",
+        "--disable-background-timer-throttling",
+        "--disable-backgrounding-occluded-windows",
+        "--disable-breakpad",
+        "--disable-client-side-phishing-detection",
+        "--disable-component-update",
+        "--disable-default-apps",
+        "--disable-dev-shm-usage",
+        "--disable-domain-reliability",
+        "--disable-extensions",
+        "--disable-features=AudioServiceOutOfProcess",
+        "--disable-hang-monitor",
+        "--disable-ipc-flooding-protection",
+        "--disable-notifications",
+        "--disable-offer-store-unmasked-wallet-cards",
+        "--disable-popup-blocking",
+        "--disable-print-preview",
+        "--disable-prompt-on-repost",
+        "--disable-renderer-backgrounding",
+        "--disable-setuid-sandbox",
+        "--disable-speech-api",
+        "--disable-sync",
+        "--hide-scrollbars",
+        "--ignore-gpu-blacklist",
+        "--metrics-recording-only",
+        "--mute-audio",
+        "--no-default-browser-check",
+        "--no-first-run",
+        "--no-pings",
+        "--no-sandbox",
+        "--no-zygote",
+        "--password-store=basic",
+        "--use-gl=swiftshader",
+        "--use-mock-keychain",
+      ],
+      timeout: 10000,
+    });
     const page = await browser.newPage();
     const dimension = 1300;
     await page.setViewport({ width: dimension, height: dimension });
@@ -74,7 +69,7 @@ async function getScreenshot(von, lastName) {
     await page.waitForSelector("#screenshot-hook", { timeout: 0 });
     const screenshotHook = await page.$("#screenshot-hook");
     const base64 = await screenshotHook.screenshot({ encoding: "base64" });
-    await page.close();
+    await browser.close();
     return `data:image/jpeg;base64,${base64}`;
   } catch (err) {
     console.error(err);
